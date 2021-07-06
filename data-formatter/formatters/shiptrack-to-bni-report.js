d8543e665f15ed5a753a94edcf808b8ab6ba8c12
Jeremy Bankes <jeremy.bankes@gmail.com>
2021-07-06 14:39:57 -0300

    Formatted date

--- data-formatter/formatters/shiptrack-to-bni-report.js
+++ data-formatter/formatters/shiptrack-to-bni-report.js
@@ -6,11 +6,15 @@ function format(input, index) {
 
     input = input.match(/".*?(?<!\\)"/g).map(cell => cell.trim().replaceAll(/^"|"$/g, ''));
 
+    let eventTime = new Date(`${input[2]} ${input[3]}`);
+    const pad = value => value.toString().padStart(2, '0');
+    eventTime = `${pad(eventTime.getUTCMonth() + 1)}/${pad(eventTime.getUTCDate())}/${eventTime.getUTCFullYear()} ${pad(eventTime.getUTCHours())}:${pad(eventTime.getUTCMinutes())}`;
+
     if (input[8].startsWith('PURO_D')) {
         lines.push([
             /* TrackingId      */ input[0],
             /* ReferenceId     */ input[1],
-            /* EventTime       */ `${input[2]} ${input[3]}`,
+            /* EventTime       */ eventTime,
             /* EventCity       */ input[4],
             /* EventProvince   */ input[5],
             /* Latitude        */ input[6],
@@ -18,10 +22,6 @@ function format(input, index) {
             /* StatusId        */ input[9],
             /* CourierId       */ 'NovaXpress 1'
         ].join(','));
-
-        console.log(lines);
-    } else {
-        console.log(input[6]);
     }
 
     return lines;
